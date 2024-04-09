073592a8a6e600e160f209516295a9292b905ea3
Thiago Bovo <149557038+Tx-Bovo@users.noreply.github.com>
2024-04-09 08:44:55 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+import socket
+import threading
+import argparse
+
+
+
+
+# Simple port Scanner
+def scan_port(target, port, open_ports, lock):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)
+            s.connect((target, port))
+            
+            with lock:
+                open_ports.append(port)
+                print(f"[+] {port} Open!")
+
+    except (socket.timeout, ConnectionRefusedError):
+        pass  # Close Ports
+
+
+# Threads to be fast
+def scan_ports_with_threads(target, init_port, end_port):
+    open_ports = []
+    lock = threading.Lock()
+    threads = []
+
+    for port in range(init_port, end_port + 1):
+        thread = threading.Thread(target=scan_port, args=(target, port, open_ports, lock))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    return open_ports
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Port Scanner')
+    parser.add_argument('-t', '--target', required=True, help='Target')
+    parser.add_argument('-a', '--all', action='store_true', help='For all ports')
+
+    args = parser.parse_args()
+    target = args.target
+    all_ports = args.all
+    if all_ports:
+        init_port = 1
+        end_port = 65535
+    else:
+        init_port = 1
+        end_port = 1024
+
+
+    try:
+        ports = scan_ports_with_threads(target, init_port, end_port)
+    except Exception as e:
+        print(e)
+        
+
+if __name__ == '__main__':
+    main()
